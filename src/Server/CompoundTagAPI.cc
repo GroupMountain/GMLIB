ec3401c064bf3cbf55904de2be59a29846148c4b
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-01-09 02:02:12 +0800

    fix: fix api error

--- src/Server/CompoundTagAPI.cc
+++ src/Server/CompoundTagAPI.cc
@@ -3,26 +3,26 @@
 
 namespace GMLIB::CompoundTagHelper {
 
-DataLoadHelper* getDataLoadHelper() {
+GMLIB_API DataLoadHelper* getDataLoadHelper() {
     return (DataLoadHelper*)ll::memory::resolveSymbol("??_7DefaultDataLoadHelper@@6B@");
 }
 
-std::unique_ptr<CompoundTag> getNbt(Actor* ac) { return ac->saveToNbt(); }
+GMLIB_API std::unique_ptr<CompoundTag> getNbt(Actor* ac) { return ac->saveToNbt(); }
 
-std::unique_ptr<CompoundTag> getNbt(Player* pl) { return pl->saveToNbt(); }
+GMLIB_API std::unique_ptr<CompoundTag> getNbt(Player* pl) { return pl->saveToNbt(); }
 
-std::unique_ptr<CompoundTag> getNbt(BlockActor* blac) { return blac->saveToNbt(); }
+GMLIB_API std::unique_ptr<CompoundTag> getNbt(BlockActor* blac) { return blac->saveToNbt(); }
 
-std::unique_ptr<CompoundTag> getNbt(ItemStack* item) { return item->save(); }
+GMLIB_API std::unique_ptr<CompoundTag> getNbt(ItemStack* item) { return item->save(); }
 
-bool setNbt(Actor* ac, CompoundTag* nbt) { return ac->load(*nbt, *getDataLoadHelper()); }
+GMLIB_API bool setNbt(Actor* ac, CompoundTag* nbt) { return ac->load(*nbt, *getDataLoadHelper()); }
 
-bool setNbt(Player* pl, CompoundTag* nbt) { return pl->load(*nbt, *getDataLoadHelper()); }
+GMLIB_API bool setNbt(Player* pl, CompoundTag* nbt) { return pl->load(*nbt, *getDataLoadHelper()); }
 
-void setNbt(BlockActor* blac, CompoundTag* nbt) {
+GMLIB_API void setNbt(BlockActor* blac, CompoundTag* nbt) {
     blac->load(*ll::service::bedrock::getLevel(), *nbt, *getDataLoadHelper());
 }
 
-void setNbt(ItemStack* item, CompoundTag* nbt) { item->load(*nbt); }
+GMLIB_API void setNbt(ItemStack* item, CompoundTag* nbt) { item->load(*nbt); }
 
 } // namespace GMLIB::CompoundTagHelper
