f0fddb13d353e24fa4776db5371cf7e084812161
EpsilonZunsat <2748716171@qq.com>
2024-01-09 12:14:21 +0800

    完成列表修改主体

--- src/Server/FakeListAPI/FakeListAPI.cc
+++ src/Server/FakeListAPI/FakeListAPI.cc
@@ -1,10 +1,13 @@
 #include "Global.h"
+#include "ll/api/service/Bedrock.h"
+#include "mc/world/actor/player/PlayerListPacketType.h"
 #include <GMLIB/Server/FakeListAPI.h>
 LoopbackPacketSender* pktSender;
 namespace GMLIB::FakeListAPI{
     std::unordered_set<std::string> invisibleMap;
     std::unordered_map<std::string, std::string> replaceMap;
     std::unordered_map<std::string, PlayerListEntry> fakeListMap;
+    bool simulatedPlayerOptList=false;
     extern void sendAddFakeListPacket(PlayerListEntry entry);
     extern void sendRemoveFakeListPacket(std::vector<PlayerListEntry> entries);
 
@@ -21,7 +24,39 @@ namespace GMLIB::FakeListAPI{
     for(auto fakeListPair:fakeListMap){
     pkt.emplace(std::move(fakeListPair.second));
     }
+    BinaryStream bs;//DefaultPermission
+    bs.writeUnsignedInt64(-1,0,0);
+    bs.writeUnsignedChar((unsigned char)1,0,0);
+    bs.writeUnsignedChar((unsigned char)CommandPermissionLevel::Any,0,0);
+    bs.writeUnsignedVarInt(0,0,0);
+    auto ablitiespkt = MinecraftPackets::createPacket(MinecraftPacketIds::UpdateAbilitiesPacket);
+    ablitiespkt->read(bs);
     pktSender->sendToClient(this->getNetworkIdentifier(),pkt,this->getClientSubId());
+    pktSender->sendToClient(this->getNetworkIdentifier(),*ablitiespkt,this->getClientSubId());
     return origin();
+}
+LL_AUTO_TYPED_INSTANCE_HOOK(
+    fakeListEmplace,
+    HookPriority::Normal,
+    PlayerListPacket,
+    "?emplace@PlayerListPacket@@QEAAX$$QEAVPlayerListEntry@@@Z",
+    void,
+    PlayerListEntry&entry)
+{
+    if(this->mAction==PlayerListPacketType::Add){
+        if (invisibleMap.count(entry.mName)) {
+            return;
+        }
+        if (simulatedPlayerOptList) {
+            //auto pl=ll::service::getLevel()->getPlayer(entry.mId); 安全性检测?
+            if (ll::service::getLevel()->getPlayer(entry.mId)->isSimulatedPlayer()) {
+                return;
+            }
+        }
+        if(replaceMap.count(entry.mName)){
+            entry.mName=replaceMap[entry.mName];
+        }
+    }
+    return origin(entry);
 }
 }
--- src/Server/FakeListAPI/ReviseList.cc
+++ src/Server/FakeListAPI/ReviseList.cc
@@ -1,5 +1,29 @@
 #include "Global.h"
 #include <GMLIB/Server/FakeListAPI.h>
-namespace GMLIB::FakeListAPI::ReviseList{
-
+namespace GMLIB::FakeListAPI{
+    extern std::unordered_map<std::string, std::string> replaceMap;
+    extern std::unordered_map<std::string, PlayerListEntry> fakeListMap;
+    extern bool simulatedPlayerOptList;
+    extern void sendAddFakeListPacket(PlayerListEntry entry);
+    void replaceList(bool add, std::string oldName, std::string newName){
+        if(add){
+            replaceMap[oldName]=newName;
+        }
+        else{
+            replaceMap.erase(oldName);
+        }
+        ll::service::getLevel()->forEachPlayer([add,oldName,newName](Player&pl) ->bool{
+            if(pl.getRealName()==oldName){
+                if(pl.isSimulatedPlayer()&&!simulatedPlayerOptList){
+                    return true;
+                }
+                auto entry=PlayerListEntry(pl);
+                sendAddFakeListPacket(entry);
+            }
+            return true;
+        });
+        if(fakeListMap.count(oldName)){
+            sendAddFakeListPacket(fakeListMap[oldName]);
+        }
+    }
 }
