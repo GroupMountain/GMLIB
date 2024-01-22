0d22a583e5d971c06a604806e60ef3982ddbaada
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-01-22 21:15:35 +0800

    fix: add missing api

--- src/Files/JsonLanguage.cc
+++ src/Files/JsonLanguage.cc
@@ -3,24 +3,38 @@
 
 namespace GMLIB::Files::JsonLanguage {
 
-void updateFile(std::string path, nlohmann::json newFile) {
+nlohmann::json initLanguage(std::string path, nlohmann::json& defaultFile) {
+    if (!std::filesystem::exists(path)) {
+        writeFile(path, defaultFile);
+    } else {
+        updateFile(path, defaultFile);
+    }
+    return readFromFile(path);
+}
+
+nlohmann::json initLanguage(std::string path, std::string& defaultFile) {
+    auto file = nlohmann::json::parse(defaultFile, nullptr, true, true);
+    return initLanguage(path, file);
+}
+
+void updateFile(std::string& path, nlohmann::json& newFile) {
     auto oldFile = readFromFile(path);
     newFile.merge_patch(oldFile);
     writeFile(path, newFile);
 }
 
-void updateFile(std::string path, std::string newFile) {
+void updateFile(std::string& path, std::string& newFile) {
     auto file = nlohmann::json::parse(newFile, nullptr, true, true);
     updateFile(path, file);
 }
 
-nlohmann::json readFromFile(std::string path) {
+nlohmann::json readFromFile(std::string& path) {
     std::ifstream inputFile(path);
     std::string   fileContent((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
     return nlohmann::json::parse(fileContent, nullptr, true, true);
 }
 
-bool writeFile(std::string path, nlohmann::json json) {
+bool writeFile(std::string& path, nlohmann::json& json) {
     try {
         std::ofstream newFile(path);
         if (!newFile.is_open()) {
@@ -34,7 +48,7 @@ bool writeFile(std::string path, nlohmann::json json) {
     }
 }
 
-std::string getValue(nlohmann::json json, std::string key) {
+std::string getValue(nlohmann::json& json, std::string key) {
     try {
         return json[key].get<std::string>();
     } catch (...) {
@@ -42,7 +56,7 @@ std::string getValue(nlohmann::json json, std::string key) {
     }
 }
 
-std::string getValue(nlohmann::json json, std::string key, std::string defaultValue) {
+std::string getValue(nlohmann::json& json, std::string key, std::string defaultValue) {
     try {
         return json[key].get<std::string>();
     } catch (...) {
@@ -50,24 +64,24 @@ std::string getValue(nlohmann::json json, std::string key, std::string defaultVa
     }
 }
 
-bool setValue(nlohmann::json json, std::string key, std::string value, std::string path) {
+bool setValue(nlohmann::json& json, std::string key, std::string value, std::string path) {
     json[key] = value;
     return writeFile(path, json);
 }
 
-bool deleteKey(nlohmann::json json, std::string key, std::string path) {
+bool deleteKey(nlohmann::json& json, std::string key, std::string path) {
     json.erase(key);
     return writeFile(path, json);
 }
 
-bool deleteKeys(nlohmann::json json, std::vector<std::string> keys, std::string path) {
+bool deleteKeys(nlohmann::json& json, std::vector<std::string> keys, std::string path) {
     for (auto& key : keys) {
         json.erase(key);
     }
     return writeFile(path, json);
 }
 
-std::string translate(nlohmann::json json, std::string key, std::vector<std::string> data, std::string translateKey) {
+std::string translate(nlohmann::json& json, std::string key, std::vector<std::string> data, std::string translateKey) {
     auto lang = getValue(json, key);
     if (data.empty()) {
         return lang;
