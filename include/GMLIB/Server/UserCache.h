0af531e45f4dc7f595fba93d8c7e2fa8a4129fa8
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-01-24 21:01:33 +0800

    feat: add userchace

---
+++ include/GMLIB/Server/UserCache.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "GMLIB/GMLIB.h"
+#include "nlohmann/json.hpp"
+
+namespace GMLIB::Server::UserCache {
+
+GMLIB_API std::optional<nlohmann::json> tryFindCahceInfoFromUuid(std::string& uuid);
+
+GMLIB_API std::optional<nlohmann::json> tryFindCahceInfoFromXuid(std::string& xuid);
+
+GMLIB_API std::optional<nlohmann::json> tryFindCahceInfoFromName(std::string& name);
+
+GMLIB_API std::optional<std::string> getXuidByUuid(std::string& uuid);
+
+GMLIB_API std::optional<std::string> getNameByUuid(std::string& uuid);
+
+GMLIB_API std::optional<std::string> getUuidByXuid(std::string& xuid);
+
+GMLIB_API std::optional<std::string> getNameByXuid(std::string& xuid);
+
+GMLIB_API std::optional<std::string> getXuidByName(std::string& name);
+
+GMLIB_API std::optional<std::string> getUuidByName(std::string& name);
+
+} // namespace GMLIB::Server
