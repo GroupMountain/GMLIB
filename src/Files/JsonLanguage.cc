1fca071a6f995fcdf3c9e56ec18510ebcbaacc48
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-02-07 20:59:10 +0800

    fix: fix json language

--- src/Files/JsonLanguage.cc
+++ src/Files/JsonLanguage.cc
@@ -4,42 +4,8 @@
 
 namespace GMLIB::Files::JsonLanguage {
 
-bool writeFile(std::string& path, nlohmann::json& json) {
-    try {
-        std::ofstream newFile(path);
-        if (!newFile.is_open()) {
-            return false;
-        }
-        newFile << json.dump(4);
-        newFile.close();
-        return true;
-    } catch (...) {
-        return false;
-    }
-}
-
-nlohmann::json readFromFile(std::string& path) {
-    std::ifstream inputFile(path);
-    std::string   fileContent((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
-    return nlohmann::json::parse(fileContent, nullptr, true, true);
-}
-
-void updateFile(std::string& path, nlohmann::json& newFile) {
-    auto oldFile = readFromFile(path);
-    newFile.merge_patch(oldFile);
-    writeFile(path, newFile);
-}
-
 nlohmann::json initLanguage(std::string path, nlohmann::json& defaultFile) {
-    auto dirPath = std::filesystem::path(path).parent_path();
-    if (!std::filesystem::exists(dirPath)) {
-        std::filesystem::create_directories(dirPath);
-    }
-    if (!std::filesystem::exists(path)) {
-        writeFile(path, defaultFile);
-    } else {
-        updateFile(path, defaultFile);
-    }
+    return JsonFile::initJson(path, defaultFile);
 }
 
 nlohmann::json initLanguage(std::string path, std::string& defaultFile) {
@@ -65,19 +31,19 @@ std::string getValue(nlohmann::json& json, std::string key, std::string defaultV
 
 bool setValue(nlohmann::json& json, std::string key, std::string value, std::string path) {
     json[key] = value;
-    return writeFile(path, json);
+    return JsonFile::writeFile(path, json);
 }
 
 bool deleteKey(nlohmann::json& json, std::string key, std::string path) {
     json.erase(key);
-    return writeFile(path, json);
+    return JsonFile::writeFile(path, json);
 }
 
 bool deleteKeys(nlohmann::json& json, std::vector<std::string> keys, std::string path) {
     for (auto& key : keys) {
         json.erase(key);
     }
-    return writeFile(path, json);
+    return JsonFile::writeFile(path, json);
 }
 
 std::string translate(nlohmann::json& json, std::string key, std::vector<std::string> data, std::string translateKey) {
