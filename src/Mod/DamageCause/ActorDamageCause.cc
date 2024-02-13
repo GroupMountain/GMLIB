58f3122dbcfc3f7f901f45869aa8eea1e7fc3fbe
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-02-13 16:51:47 +0800

    feat: damage cause

--- src/Mod/DamageCause/ActorDamageCause.cc
+++ src/Mod/DamageCause/ActorDamageCause.cc
@@ -8,12 +8,49 @@ namespace GMLIB::Mod {
 using DEATH_MESSAGE = std::pair<std::string, std::vector<std::string>>;
 using namespace ll::chrono_literals;
 
-std::unordered_map<int, std::string>     mDamageCauseDefinition;
 std::unordered_map<int64, int>           mFallHeightMap;
 std::unordered_map<int64, ActorUniqueID> mHurtByEntityMap;
 bool                                     mDamageCauseDefinitionEnabled = false;
+int                                      mMaxCauseId                   = 34;
+std::vector<std::pair<std::string, int>> mCustomCauseMap;
 
 bool isCrystal = false;
+
+bool isCustomCauseExist(std::string& causeName) {
+    for (auto& key : mCustomCauseMap) {
+        if (key.first == causeName) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isCustomCauseExist(int cause) {
+    for (auto& key : mCustomCauseMap) {
+        if (key.second == cause) {
+            return true;
+        }
+    }
+    return false;
+}
+
+::ActorDamageCause getCustomCause(std::string& causeName) {
+    for (auto& key : mCustomCauseMap) {
+        if (key.first == causeName) {
+            return (ActorDamageCause)key.second;
+        }
+    }
+    return ActorDamageCause::None;
+}
+
+std::string getCustomCause(::ActorDamageCause cause) {
+    for (auto& key : mCustomCauseMap) {
+        if ((ActorDamageCause)key.second == cause) {
+            return key.first;
+        }
+    }
+    return "none";
+}
 
 Actor* getDamagingEntity(ActorDamageSource* ads) {
     auto id = ads->getDamagingEntityUniqueID();
@@ -42,9 +79,13 @@ DEATH_MESSAGE makeDeathMessage(
     bool           isHardCodedMessage = false
 ) {
     // 自定义类型构造
-    if (mDamageCauseDefinition.count(cause) || isHardCodedMessage) {
+    if (cause >= 35 || isHardCodedMessage) {
         std::string msg = "death.attack.damageCause.item";
-        ll::utils::string_utils::replaceAll(msg, "damageCause", mDamageCauseDefinition[cause]);
+        ll::utils::string_utils::replaceAll(
+            msg,
+            "damageCause",
+            getCustomCause(ActorDamageCause(cause))
+        );
         if (isHardCodedMessage) {
             msg = deathMessage.first;
         }
@@ -402,14 +443,21 @@ void DamageCause::setCustomDamageCauseEnabled() {
         ll::memory::HookRegistrar<DeathMessageHook3>().hook();
         ll::memory::HookRegistrar<DeathMessageHook4>().hook();
         ll::memory::HookRegistrar<MobDieHook>().hook();
-        ll::memory::HookRegistrar<MobDieHook>().hook();
+        ll::memory::HookRegistrar<CryatslHurtHook>().hook();
         mDamageCauseDefinitionEnabled = true;
     }
 }
 
-bool DamageCause::registerDamageCause(int causeId, std::string causeName) {
-    if (mDamageCauseDefinition.count(causeId)) {
-        mDamageCauseDefinition[causeId] = causeName;
+int getNextCauseId() {
+    mMaxCauseId++;
+    return mMaxCauseId;
+}
+
+bool DamageCause::registerDamageCause(std::string causeName) {
+    if (!isCustomCauseExist(causeName)) {
+        setCustomDamageCauseEnabled();
+        auto id = getNextCauseId();
+        mCustomCauseMap.push_back({causeName, id});
         return true;
     }
     return false;
