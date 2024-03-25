df118bad885bb4f1407400a6afa4edda60493b22
Tsubasa6848 <komatsu_moho@outlook.com>
2024-03-25 17:39:38 +0800

    feat: add ResourcePackInitEvent

--- include/GMLIB/Event/Registries/ResourcePackInitEvent.h
+++ include/GMLIB/Event/Registries/ResourcePackInitEvent.h
@@ -1,3 +1,18 @@
 #pragma once
 #include "GMLIB/GMLIB.h"
 #include "ll/api/event/Event.h"
+#include "mc/resources/ResourcePackRepository.h"
+
+namespace GMLIB::Event::Registries {
+
+class ResourcePackInitEvent : public ll::event::Event {
+protected:
+    ResourcePackRepository& mRepo;
+
+public:
+    constexpr explicit ResourcePackInitEvent(ResourcePackRepository& repo) : mRepo(repo) {}
+
+    GMLIB_API ResourcePackRepository& getResourcePackRepository() const;
+};
+
+} // namespace GMLIB::Event::Registries
--- src/Event/Registries/ResourcePackInitEvent.cc
+++ src/Event/Registries/ResourcePackInitEvent.cc
@@ -1,2 +1,29 @@
 #include "Global.h"
 #include <GMLIB/Event/Registries/ResourcePackInitEvent.h>
+
+namespace GMLIB::Event::Registries {
+
+ResourcePackRepository& ResourcePackInitEvent::getResourcePackRepository() const { return mRepo; }
+
+LL_TYPE_INSTANCE_HOOK(
+    ResourcePackInitEventHook,
+    ll::memory::HookPriority::Normal,
+    ResourcePackRepository,
+    "?_initialize@ResourcePackRepository@@AEAAXXZ",
+    void
+) {
+    auto event = ResourcePackInitEvent(*this);
+    ll::event::EventBus::getInstance().publish(event);
+    return origin();
+}
+
+static std::unique_ptr<ll::event::EmitterBase> emitterFactory(ll::event::ListenerBase&);
+class ResourcePackInitEventEmitter : public ll::event::Emitter<emitterFactory, ResourcePackInitEvent> {
+    ll::memory::HookRegistrar<ResourcePackInitEventHook> hook;
+};
+
+static std::unique_ptr<ll::event::EmitterBase> emitterFactory(ll::event::ListenerBase&) {
+    return std::make_unique<ResourcePackInitEventEmitter>();
+}
+
+} // namespace GMLIB::Event::Registries
