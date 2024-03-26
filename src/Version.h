85b3db0b490cf9f302221457ad993f534d87b1b8
Tsubasa6848 <komatsu_moho@outlook.com>
2024-03-27 03:01:05 +0800

    chore: update version info

--- src/Version.h
+++ src/Version.h
@@ -3,12 +3,12 @@
 #define LIB_NAME fmt::format(fg(fmt::color::light_pink), "GMLIB")
 
 #define LIB_VERSION_MAJOR      0
-#define LIB_VERSION_MINOR      9
-#define LIB_VERSION_REVISION   8
+#define LIB_VERSION_MINOR      10
+#define LIB_VERSION_REVISION   0
 #define LIB_VERSION_PRERELEASE ""
 #define LIB_VERSION_BUILD_META ""
 
-#define TARGET_PROTOCOL_VERSION 649
+#define TARGET_PROTOCOL_VERSION 662
 
 #define LIB_AUTHOR     "GroupMountain"
 #define LIB_REPOSITORT "https://github.com/GroupMountain/GMLIB"
