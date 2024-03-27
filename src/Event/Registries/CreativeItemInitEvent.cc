742a59221a3f2c329a3a5e03aa01f59b331c6940
Tsubasa6848 <komatsu_moho@outlook.com>
2024-03-27 19:09:57 +0800

    feat: add CreativeItemInitEvent

---
+++ src/Event/Registries/CreativeItemInitEvent.cc
@@ -0,0 +1,29 @@
+#include "Global.h"
+#include <GMLIB/Event/Registries/CreativeItemsInitEvent.h>
+
+namespace GMLIB::Event::Registries {
+
+LL_STATIC_HOOK(
+    CreativeItemInitEventHook,
+    ll::memory::HookPriority::Normal,
+    "?addLooseCreativeItems@Item@@SAX_NAEBVBaseGameVersion@@VItemRegistryRef@@@Z",
+    void,
+    bool                         a1,
+    class BaseGameVersion const& a2,
+    class ItemRegistryRef&       a3
+) {
+    auto event = CreativeItemInitEvent();
+    ll::event::EventBus::getInstance().publish(event);
+    return origin(a1, a2, a3);
+}
+
+static std::unique_ptr<ll::event::EmitterBase> emitterFactory(ll::event::ListenerBase&);
+class CreativeItemInitEventEmitter : public ll::event::Emitter<emitterFactory, CreativeItemInitEvent> {
+    ll::memory::HookRegistrar<CreativeItemInitEventHook> hook;
+};
+
+static std::unique_ptr<ll::event::EmitterBase> emitterFactory(ll::event::ListenerBase&) {
+    return std::make_unique<CreativeItemInitEventEmitter>();
+}
+
+} // namespace GMLIB::Event::Registries
