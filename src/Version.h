4a6f96ee3f28bd40e03ef28ddf1a8ac7a404c612
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-04-15 15:01:37 +0800

    chore: update version

--- src/Version.h
+++ src/Version.h
@@ -3,8 +3,8 @@
 #define LIB_NAME fmt::format(fg(fmt::color::light_pink), "GMLIB")
 
 #define LIB_VERSION_MAJOR      0
-#define LIB_VERSION_MINOR      10
-#define LIB_VERSION_REVISION   3
+#define LIB_VERSION_MINOR      11
+#define LIB_VERSION_REVISION   0
 #define LIB_VERSION_PRERELEASE ""
 #define LIB_VERSION_BUILD_META ""
 
