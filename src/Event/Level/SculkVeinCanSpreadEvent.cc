07df97a9bcc8aba5005c40a59252eecf3b0aba2f
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-07-20 23:36:48 +0800

    feat: add optional_ref<LevelChunk> to SculkVeinCanSpreadEvent

--- src/Event/Level/SculkVeinCanSpreadEvent.cc
+++ src/Event/Level/SculkVeinCanSpreadEvent.cc
@@ -3,6 +3,7 @@
 #include "ll/api/event/EmitterBase.h"
 #include "ll/api/event/EventBus.h"
 #include "ll/api/memory/Hook.h"
+#include "mc/world/level/ChunkPos.h"
 #include "mc/world/level/IBlockWorldGenAPI.h"
 #include "mc/world/level/block/utils/SculkVeinMultifaceSpreader.h"
 
@@ -10,10 +11,10 @@
 namespace GMLIB::Event::LevelEvent {
 
 
-Block const&    SculkVeinCanSpreadEvent::getBlock() const { return mBlock; }
-BlockPos const& SculkVeinCanSpreadEvent::getPos() const { return mPos; }
-uchar           SculkVeinCanSpreadEvent::getFace() const { return mFace; }
-
+Block const&             SculkVeinCanSpreadEvent::getBlock() const { return mBlock; }
+BlockPos const&          SculkVeinCanSpreadEvent::getPos() const { return mPos; }
+uchar                    SculkVeinCanSpreadEvent::getFace() const { return mFace; }
+optional_ref<LevelChunk> SculkVeinCanSpreadEvent::getLevelChunk() const { return mLevelChunk; }
 
 LL_TYPE_INSTANCE_HOOK(
     SculkVeinCanSpreadEventHook,
@@ -26,7 +27,8 @@ LL_TYPE_INSTANCE_HOOK(
     BlockPos const&    a3,
     uchar              a4
 ) {
-    auto ev = SculkVeinCanSpreadEvent(a2, a3, a4);
+    auto levelChunkPtr = a1.getChunk(ChunkPos(a3));
+    auto ev            = SculkVeinCanSpreadEvent(a2, a3, a4, levelChunkPtr);
     ll::event::EventBus::getInstance().publish(ev);
     if (ev.isCancelled()) {
         return false;
